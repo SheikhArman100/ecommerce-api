53d5744c541fe0cca24017809f68b5e38d2b11d7
SheikhArman100 <sheikharman100@gmail.com>
2025-12-25 17:32:08 +0600

    cascade migration added

---
+++ prisma/migrations/20251225112516_fix_cascade/migration.sql
@@ -0,0 +1,68 @@
+-- DropForeignKey
+ALTER TABLE `cart_items` DROP FOREIGN KEY `cart_items_productId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `cart_items` DROP FOREIGN KEY `cart_items_productId_flavorId_sizeId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `order_items` DROP FOREIGN KEY `order_items_productId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `order_items` DROP FOREIGN KEY `order_items_productId_flavorId_sizeId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `product_flavor_sizes` DROP FOREIGN KEY `product_flavor_sizes_productId_flavorId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `product_flavor_sizes` DROP FOREIGN KEY `product_flavor_sizes_sizeId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `productflavors` DROP FOREIGN KEY `productFlavors_flavorId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `productflavors` DROP FOREIGN KEY `productFlavors_productId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `products` DROP FOREIGN KEY `products_categoryId_fkey`;
+
+-- DropIndex
+DROP INDEX `cart_items_productId_flavorId_sizeId_fkey` ON `cart_items`;
+
+-- DropIndex
+DROP INDEX `order_items_productId_flavorId_sizeId_fkey` ON `order_items`;
+
+-- DropIndex
+DROP INDEX `product_flavor_sizes_sizeId_fkey` ON `product_flavor_sizes`;
+
+-- DropIndex
+DROP INDEX `productFlavors_flavorId_fkey` ON `productflavors`;
+
+-- DropIndex
+DROP INDEX `products_categoryId_fkey` ON `products`;
+
+-- AddForeignKey
+ALTER TABLE `users` ADD CONSTRAINT `users_updatedBy_fkey` FOREIGN KEY (`updatedBy`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `products` ADD CONSTRAINT `products_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `sizes` ADD CONSTRAINT `sizes_createdBy_fkey` FOREIGN KEY (`createdBy`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `productFlavors` ADD CONSTRAINT `productFlavors_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `productFlavors` ADD CONSTRAINT `productFlavors_flavorId_fkey` FOREIGN KEY (`flavorId`) REFERENCES `flavors`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `product_flavor_sizes` ADD CONSTRAINT `product_flavor_sizes_productId_flavorId_fkey` FOREIGN KEY (`productId`, `flavorId`) REFERENCES `productFlavors`(`productId`, `flavorId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `product_flavor_sizes` ADD CONSTRAINT `product_flavor_sizes_sizeId_fkey` FOREIGN KEY (`sizeId`) REFERENCES `sizes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `cart_items` ADD CONSTRAINT `cart_items_productId_flavorId_sizeId_fkey` FOREIGN KEY (`productId`, `flavorId`, `sizeId`) REFERENCES `product_flavor_sizes`(`productId`, `flavorId`, `sizeId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `order_items` ADD CONSTRAINT `order_items_productId_flavorId_sizeId_fkey` FOREIGN KEY (`productId`, `flavorId`, `sizeId`) REFERENCES `product_flavor_sizes`(`productId`, `flavorId`, `sizeId`) ON DELETE RESTRICT ON UPDATE CASCADE;
