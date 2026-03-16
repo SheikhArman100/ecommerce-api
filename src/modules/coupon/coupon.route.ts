3334061d4492b673d4cec6b5e637aab1ba9b88a8
Sheikh Arman <sheikharman@softograph.com>
2026-03-16 15:05:38 +0600

    payment fixed

--- src/modules/coupon/coupon.route.ts
+++ src/modules/coupon/coupon.route.ts
@@ -7,7 +7,7 @@ import { CouponValidation } from './coupon.validation';
 
 const router = express.Router();
 
-router.get('/', auth(ENUM_USER_ROLE.ADMIN), CouponController.getAllCoupons);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.USER), CouponController.getAllCoupons);
 
 router.post(
   '/validate',
